3c865ae29757af18a7a3ed64594134e7fd0b8cb5
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-08-15 21:14:05 +0700

    Update responsive sidebar.

--- public/js/students.js
+++ public/js/students.js
@@ -13,3 +13,13 @@ function sidebarSectionTogglerButton() {
     navTogglerButton.classList.toggle("click");
     studentSidebar.classList.toggle("click");
 }
+
+document.addEventListener("keydown", function (event) {
+    if (navTogglerButton.classList.contains("click") && event.which == 27) {
+        navTogglerButton.classList.remove("click");
+    }
+
+    if (studentSidebar.classList.contains("click") && event.which == 27) {
+        studentSidebar.classList.remove("click");
+    }
+})
