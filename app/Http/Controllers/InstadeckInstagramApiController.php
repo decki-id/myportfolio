3e79fc2aa5bb5ce951e412cce120d1761885a878
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-09 16:16:33 +0700

    Instagram API test.

--- app/Http/Controllers/InstadeckInstagramApiController.php
+++ app/Http/Controllers/InstadeckInstagramApiController.php
@@ -7,10 +7,10 @@ use GuzzleHttp\Client;
 
 class InstadeckInstagramApiController extends Controller
 {
-    const $appId = config('services.facebook.client_id');
-    const $secret = config('services.facebook.client_secret');
-    const $redirectUri = config('services.facebook.redirect_uri');
-    const $instaId = config('services.facebook.instagram_id');
+    $appId = config('services.facebook.client_id');
+    $secret = config('services.facebook.client_secret');
+    $redirectUri = config('services.facebook.redirect_uri');
+    $instaId = config('services.facebook.instagram_id');
 
     public function index()
     {
