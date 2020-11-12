818f823093434ce617c4b4ca3b2214b954970c82
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-12 16:00:41 +0700

    Instagram API test.

--- resources/views/instadeck/profile.blade.php
+++ resources/views/instadeck/profile.blade.php
@@ -156,6 +156,15 @@
                     @endforeach
                 @endfor
             </div>
+            <nav class="row" aria-label="Page navigation example">
+                <ul class="pagination text-center">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
         </div>
     @endif
 @endsection
