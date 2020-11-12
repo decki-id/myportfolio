32db7fa95dabc44aa5c850170abde48e33a167b1
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-12 11:09:02 +0700

    Instagram API test.

--- resources/views/instadeck/profile.blade.php
+++ resources/views/instadeck/profile.blade.php
@@ -139,8 +139,8 @@
                 <div><a href="{{ $profile->website }}" class="dhs_link">{{ $profile->website }}</a></div>
             </div>
             @for($a = 0; $a <= $media["data"]; $a++)
-                @foreach($media["data"][$a] as $post)
-                    {{ dd($post["media_url"]) }}
+                @foreach($media["data"][$a] as $post["data"][$a])
+                    {{ dd($post["data"][$a]["media_url"]) }}
                     <div class="col-4 pb-4">
                         <a href="#">
                             <img src="{{ $post["media_url"] }}" class="w-100">
