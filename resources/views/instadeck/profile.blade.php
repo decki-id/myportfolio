998c1b2c164e0c3cb734dde273226efa41bbb33a
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-13 09:45:05 +0700

    Instagram API test.

--- resources/views/instadeck/profile.blade.php
+++ resources/views/instadeck/profile.blade.php
@@ -150,9 +150,9 @@
                             <div class="col-4 pb-4">
                                 <a href="#">
                                     @if($posts[$a]['media_type'] == 'IMAGE' || $posts[$a]['media_type'] == 'CAROUSEL_ALBUM')
-                                        <img src="{{ $posts[$a]['media_url'] }}" class="w-100 h-100">
+                                        <img src="{{ $posts[$a]['media_url'] }}" class="w-100 h-auto">
                                     @else
-                                        <video class="w-100 h-100" controls>
+                                        <video class="w-100 h-auto" controls>
                                             <source src="{{ $posts[$a]['media_url'] }}">
                                         </video>
                                     @endif
