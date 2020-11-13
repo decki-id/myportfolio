d1303aac12702b83a8e32d9992f842b471b9412c
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-13 15:09:45 +0700

    Instagram API test.

--- app/Http/Controllers/InstadeckInstagramApiController.php
+++ app/Http/Controllers/InstadeckInstagramApiController.php
@@ -43,7 +43,7 @@ class InstadeckInstagramApiController extends Controller
         $mediaData = $getMedia->getBody()->getContents();
         $media = json_decode($mediaData, true);
 
-        dd($media['data'][0]['children']['data'][0]['id']);
+        dd($media);
 
         return view('/instadeck/profile', compact('profile', 'media'));
     }
