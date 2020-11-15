7e2bc3759fee47e58389f79f4356e8f95ee33679
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-15 23:30:52 +0700

    Instagram API test.

--- resources/views/instadeck/profile.blade.php
+++ resources/views/instadeck/profile.blade.php
@@ -160,30 +160,30 @@
                     @foreach($media as $posts)
                         @if(isset($posts[$a]))
                             <div class="col-4 pb-4 dhs_posts-item">
-                                <a href="">
-                                    <div class="dhs_posts-item-inner w-100 h-100">
-                                        @if($posts[$a]['media_type'] == 'CAROUSEL_ALBUM')
-                                            <div class="dhs_carousel-icon">
-                                                <i class="fas fa-fw fa-images text-white"></i>
-                                            </div>
-                                        @endif
-                                        @if($posts[$a]['media_type'] == 'IMAGE' || $posts[$a]['media_type'] == 'CAROUSEL_ALBUM')
-                                            <div class="dhs_posts w-100 h-100">
-                                                <img src="{{ $posts[$a]['media_url'] }}" class="w-100 h-100">
-                                            </div>
-                                        @else
-                                            <div class="dhs_posts w-100 h-100">
-                                                <video class="w-100 h-100" preload="auto" autobuffer playsinline controls>
-                                                    <source src="{{ $posts[$a]['media_url'] }}">
-                                                </video>
-                                            </div>
-                                        @endif
-                                        <div class="dhs_posts-info text-white d-flex justify-content-center align-items-center">
-                                            <div class="mr-3"><i class="fas fa-fw fa-heart mr-1"></i>{{ $posts[$a]['like_count'] }}</div>
-                                            <div class="ml-3"><i class="fas fa-fw fa-comment mr-1"></i>{{ $posts[$a]['comments_count'] }}</div>
+                                <div class="dhs_posts-item-inner w-100 h-100">
+                                    @if($posts[$a]['media_type'] == 'CAROUSEL_ALBUM')
+                                        <div class="dhs_carousel-icon">
+                                            <i class="fas fa-fw fa-images text-white"></i>
                                         </div>
+                                    @endif
+                                    @if($posts[$a]['media_type'] == 'IMAGE' || $posts[$a]['media_type'] == 'CAROUSEL_ALBUM')
+                                        <div class="dhs_posts w-100 h-100">
+                                            <a href="#">
+                                                <img src="{{ $posts[$a]['media_url'] }}" class="w-100 h-100">
+                                            </a>
+                                        </div>
+                                    @else
+                                        <div class="dhs_posts w-100 h-100">
+                                            <video class="w-100 h-100" preload="auto" autobuffer playsinline controls>
+                                                <source src="{{ $posts[$a]['media_url'] }}">
+                                            </video>
+                                        </div>
+                                    @endif
+                                    <div class="dhs_posts-info text-white d-flex justify-content-center align-items-center">
+                                        <div class="mr-3"><i class="fas fa-fw fa-heart mr-1"></i>{{ $posts[$a]['like_count'] }}</div>
+                                        <div class="ml-3"><i class="fas fa-fw fa-comment mr-1"></i>{{ $posts[$a]['comments_count'] }}</div>
                                     </div>
-                                </a>
+                                </div>
                             </div>
                         @endif
                     @endforeach
