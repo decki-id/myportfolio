c61e86810301c007f03c1d662e0aaee4a1e1cc3d
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-15 21:52:58 +0700

    Instagram API test.

--- resources/views/instadeck/profile.blade.php
+++ resources/views/instadeck/profile.blade.php
@@ -172,7 +172,7 @@
                                             </video>
                                         </div>
                                     @endif
-                                    <div class="dhs_posts-info">
+                                    <div class="dhs_posts-info text-white justify-content-center">
                                         <i class="fas fa-fw fa-heart mr-1"></i>{{ $posts[$a]['like_count'] }}
                                         <i class="fas fa-fw fa-comment mr-1"></i>{{ $posts[$a]['comments_count'] }}
                                     </div>
