6ee9c487143d2eb3cc6b0899a9f24ba3d2b1f504
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-22 18:19:35 +0700

    InstaDeck Unsplash API test.

--- resources/views/instadeck/explore.blade.php
+++ resources/views/instadeck/explore.blade.php
@@ -12,14 +12,14 @@
         </div>
         <div class="row pt-4">
             @foreach($posts as $post)
-                <div class="col-4">
+                <div class="col-4 pb-4">
                     <a href="/instadeck/post/{{ $post->id }}">
                         <img src="/storage/{{ $post->image }}" class="w-100 h-100">
                     </a>
                 </div>
             @endforeach
         </div>
-        <div class="row pt-4">
+        <div class="row">
             <div class="col-12 pb-4">
                 <a href="{{ $unsplashApi[0]['urls']['regular'] }}">
                     <img src="{{ $unsplashApi[0]['urls']['regular'] }}" class="w-100 h-100">
