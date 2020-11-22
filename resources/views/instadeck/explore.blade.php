df4bc2f7fe6c0e2105992f184384d1edaabc4535
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-22 16:29:06 +0700

    InstaDeck Unsplash API test.

--- resources/views/instadeck/explore.blade.php
+++ resources/views/instadeck/explore.blade.php
@@ -11,26 +11,15 @@
             </div>
         </div>
         <div class="row pt-4">
-            @if(!empty($posts))
-                @foreach($posts as $post)
+            @for($a = 0; $a < count($unsplashApi); $a++)
+                @foreach($unsplashApi as $unsplash)
                     <div class="col-4 pb-4">
-                        <a href="/instadeck/post/{{ $post->id }}">
-                            <img src="/storage/{{ $post->image }}" class="w-100">
+                        <a href="{{ $unsplash[$a]['urls']['raw'] . '&w=800&h=800' }}">
+                            <img src="{{ $unsplash[$a]['urls']['raw'] . '&w=800&h=800' }}" class="w-100">
                         </a>
                     </div>
                 @endforeach
-            @else
-                @for($a = 0; $a < count($unsplashApi); $a++)
-                    {{ dd($unsplashApi) }}
-                    @foreach($unsplashApi as $unsplash)
-                        <div class="col-4 pb-4">
-                            <a href="{{ $unsplash[$a]['urls']['raw'] . '&w=800&h=800' }}">
-                                <img src="{{ $unsplash[$a]['urls']['raw'] . '&w=800&h=800' }}" class="w-100">
-                            </a>
-                        </div>
-                    @endforeach
-                @endfor
-            @endif
+            @endfor
         </div>
     </div>
 @endsection
