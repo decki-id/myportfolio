8401a4750400fb2eafca88b559e971ba773b4002
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-11-22 18:14:06 +0700

    InstaDeck Unsplash API test.

--- resources/views/instadeck/explore.blade.php
+++ resources/views/instadeck/explore.blade.php
@@ -10,22 +10,19 @@
                 <span class="input-group-text"><i class="fas fa-fw fa-search"></i></span>
             </div>
         </div>
-        @if(!empty($posts))
-            <div class="row pt-4">
-                @foreach($posts as $post)
-                    <div class="col-4 pb-4">
-                        <a href="/instadeck/post/{{ $post->id }}">
-                            <img src="/storage/{{ $post->image }}" class="w-100 h-100">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-        @else
-            <div class="row pt-4">
-                <a href="{{ $unsplashApi[$a]['urls']['regular'] }}">
-                    <img src="{{ $unsplashApi[$a]['urls']['regular'] }}" class="w-100 h-100">
-                </a>
-            </div>
-        @endif
+        <div class="row pt-4">
+            @foreach($posts as $post)
+                <div class="col-4 pb-4">
+                    <a href="/instadeck/post/{{ $post->id }}">
+                        <img src="/storage/{{ $post->image }}" class="w-100 h-100">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        <div class="row pt-4">
+            <a href="{{ $unsplashApi[0]['urls']['regular'] }}">
+                <img src="{{ $unsplashApi[0]['urls']['regular'] }}" class="w-100 h-100">
+            </a>
+        </div>
     </div>
 @endsection
