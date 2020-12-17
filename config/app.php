4f212722528bf9e6dedc30c72cae26aeab654fd5
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2020-12-17 08:41:56 +0700

    Starting new project: SisDeck.

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
 
-        Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
         InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
