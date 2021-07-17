f7a8624b6f89be20e265159d898372c3ed907f1f
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2021-07-17 19:13:42 +0700

    Update SisDeck.

--- app/Http/Controllers/InstadeckInstagramApiController.php
+++ app/Http/Controllers/InstadeckInstagramApiController.php
@@ -23,8 +23,9 @@ class InstadeckInstagramApiController extends Controller
         $secret = config('services.facebook.client_secret');
         $redirectUri = config('services.facebook.redirect_uri');
         $code = $request->code;
+        dd($code);
         
-        // if (empty($code)) return redirect()->route('instadeck.home')->with('error', 'Failed to login with Instagram.');
+        if (empty($code)) return redirect()->route('instadeck.home')->with('error', 'Failed to login with Instagram.');
 
         try {
             $response = $client->request('GET', "http://graph.facebook.com/v9.0/oauth/access_token?client_id={$appId}&client_secret={$secret}&redirect_uri={$redirectUri}&code={$code}");
