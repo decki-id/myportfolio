d31fbc3fe25fb281abaf99c558c0223cb2013b9f
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2021-07-21 19:13:56 +0700

    About Me page update.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -95,29 +95,32 @@
                                 <h3 class="edex-title padd-15">Experience</h3><br>
                                 <div class="timeline-item">
                                     <div class="circle-dot"></div>
-                                    <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>10 Juni 2021 - Present</h6>
-                                    <h4 class="timeline-title"><span>Full Stack Web Developer</span> at Kamar Pelajar</h4>
-                                    <p class="timeline-subtitle">Internship</p>
+                                    <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>10 June 2021 - Present</h6>
+                                    <h4 class="timeline-title"><span>Full Stack Web Developer</span></h4>
+                                    <h4 class="timeline-title">Kamar Pelajar || Internship (freelance remote work)</h4>
+                                    <p class="timeline-subtitle">An online marketplace startup for lodging, primary homestays for vacation rentals, and tourism activities.</p>
                                     <ul class="timeline-text">
-                                        <li>Freelance remote work</li>
+                                        <li>Web development using Laravel 7, Bootstrap 5, and MySQL</li>
                                     </ul>
                                 </div>
                                 <div class="timeline-item">
                                     <div class="circle-dot"></div>
                                     <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>3 January 2018 - Present</h6>
-                                    <h4 class="timeline-title"><span>Customer Support</span> at PT. Ava Revota</h4>
-                                    <p class="timeline-subtitle">IT consultant for apparel industry based in Bandung city.</p>
+                                    <h4 class="timeline-title"><span>Customer Support</span></h4>
+                                    <h4 class="timeline-title">PT. Ava Revota || Full Time</h4>
+                                    <p class="timeline-subtitle">An IT consultant company for the apparel industry based in Bandung city.</p>
                                     <ul class="timeline-text">
-                                        <li>System installation and configuration</li>
-                                        <li>Database administrator</li>
-                                        <li>Problem troubleshooting</li>
+                                        <li>System configuration and troubleshooting using Visual Basic 6</li>
+                                        <li>Database management using Microsoft SQL Server 2000</li>
+                                        <li>Research and development using Python (Flask and FastAPI framework) and PostgreSQL</li>
                                     </ul>
                                 </div>
                                 <div class="timeline-item">
                                     <div class="circle-dot"></div>
                                     <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>February - Juli 2017</h6>
-                                    <h4 class="timeline-title"><span>Logistic Administrator</span> at CV. Surya Glass</h4>
-                                    <p class="timeline-subtitle">Distribution for every kind of glasses.</p>
+                                    <h4 class="timeline-title"><span>Logistic Administrator</span></h4>
+                                    <h4 class="timeline-title">CV. Surya Glass || Full Time</h4>
+                                    <p class="timeline-subtitle">Distributor for every kind of glasses.</p>
                                     <ul class="timeline-text">
                                         <li>Data entry</li>
                                         <li>Stock keeping</li>
@@ -127,10 +130,11 @@
                                 <div class="timeline-item">
                                     <div class="circle-dot"></div>
                                     <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>October 2014 - January 2016</h6>
-                                    <h4 class="timeline-title"><span>Engineering Staff</span> at Kartika Sari</h4>
+                                    <h4 class="timeline-title"><span>Engineering Staff</span></h4>
+                                    <h4 class="timeline-title">Kartika Sari || Full Time (shift)</h4>
                                     <p class="timeline-subtitle">Food and beverages industry.</p>
                                     <ul class="timeline-text">
-                                        <li>Maintenance of Air Conditioners / Cooling Machines, Electricity and Plumbing Installations</li>
+                                        <li>Maintenance of Air Conditioners/Cooling Machines, Electricity, and Plumbing Installations</li>
                                         <li>Build electricity and plumbing installation in construction projects</li>
                                         <li>Repair electronic stuff</li>
                                     </ul>
@@ -138,7 +142,8 @@
                                 <div class="timeline-item item-last-child">
                                     <div class="circle-dot"></div>
                                     <h6 class="timeline-date"><i class="fas fa-fw fa-calendar-alt"></i>March 2013 - January 2014</h6>
-                                    <h4 class="timeline-title"><span>Logistic Administrator</span> at PT. Bersama Zatta Jaya (Elzatta brand)</h4>
+                                    <h4 class="timeline-title"><span>Logistic Administrator</span></h4>
+                                    <h4 class="timeline-title">PT. Bersama Zatta Jaya (Elzatta brand) || Full Time</h4>
                                     <p class="timeline-subtitle">Apparel industry.</p>
                                     <ul class="timeline-text">
                                         <li>Data entry</li>
