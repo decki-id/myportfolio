bd5b51f7923a24e76d74a34224b36e5a54e8d47f
unknown <d.herdiawan.s@gmail.com>
2021-11-24 10:26:38 +0700

    Update Teachers menu.

--- resources/views/sisdeck/teachers/update.blade.php
+++ resources/views/sisdeck/teachers/update.blade.php
@@ -1,23 +1,47 @@
-@extends('layouts.app')
-
-@section('content')
-    <section class="content-header">
-        <h1>
-            Sisdeck Teacher
-        </h1>
-   </section>
-   <div class="content">
-       @include('adminlte-templates::common.errors')
-       <div class="box box-primary">
-           <div class="box-body">
-               <div class="row">
-                   {!! Form::model($sisdeckTeacher, ['route' => ['sisdeckTeachers.update', $sisdeckTeacher->id], 'method' => 'patch']) !!}
-
-                        @include('sisdeck_teachers.fields')
-
-                   {!! Form::close() !!}
-               </div>
-           </div>
-       </div>
-   </div>
-@endsection
+<!-- Update Teacher Modal -->
+<div class="modal fade" id="update-teacher_modal" tabindex="-1" role="dialog" aria-labelledby="updateTeacherHeaderModal" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-ms modal-right" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-warning text-white align-items-center">
+                <h5 class="modal-title" id="updateTeacherHeaderModal">Update Teacher</h5>
+                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            {!! Form::open(['route' => ['sisdeck.teachers.update', 'id'], 'method' => 'POST']) !!}
+            @csrf
+            @method('PATCH')
+            <div class="modal-body">
+                {!! Form::hidden('id', null, ['id' => 'id']) !!}
+                <div class="row align-items-center">
+                    <div class="form-group col-sm-12">
+                        {!! Form::label('username', 'Username', ['class' => 'form-label']) !!}
+                        {!! Form::text('username', null, ['class' => 'form-control', 'maxlength' => 191, 'id' => 'username-update', 'title' => 'Username', 'placeholder' => 'Insert username']) !!}
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="form-group col-sm-12">
+                        {!! Form::label('fullname', 'Fullname', ['class' => 'form-label']) !!}
+                        {!! Form::text('fullname', null, ['class' => 'form-control', 'maxlength' => 191, 'id' => 'fullname-update', 'title' => 'Fullname', 'placeholder' => 'Insert fullname']) !!}
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="form-group col-sm-12">
+                        {!! Form::label('role_id', 'Role', ['class' => 'form-label']) !!}
+                        {!! Form::select('role_id', $role, 'null', ['class' => 'form-control', 'maxlength' => 191, 'id' => 'role-update', 'title' => 'User Role', 'placeholder' => 'Choose role']) !!}
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="form-group col-sm-12">
+                        {!! Form::label('email', 'Email', ['class' => 'form-label']) !!}
+                        {!! Form::email('email', null, ['class' => 'form-control', 'maxlength' => 191, 'id' => 'email-update', 'title' => 'Email', 'placeholder' => 'Insert email']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                {!! Form::submit('Update', ['class' => 'btn btn-warning text-white']) !!}
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+<!-- End of Update Teacher Modal -->
