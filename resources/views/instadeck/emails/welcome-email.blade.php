a38788e33f01cd10b3434f65874db54a5eb529ea
unknown <d.herdiawan.s@gmail.com>
2022-01-18 08:59:03 +0700

    Setup InstaDeck project as subdomain.

--- resources/views/instadeck/emails/welcome-email.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# Welcome to InstaDeck!
-
-This is one of the portfolio websites made by Decki Herdiawan Soepandi and I thank you for checking this one out.<br>
-Go check another portfolio in <a href="https://deckiherdiawans.com" style="text-decoration: none;">deckiherdiawans.com</a>!
-
-Best regards,<br>
-{{ env('MAIL_FROM_NAME') }}
-@endcomponent
